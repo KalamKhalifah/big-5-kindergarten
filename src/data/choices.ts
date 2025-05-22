61810c2aa95520223055dcd0c956d8341568226e
KalamKhalifah <bbtunjong@littlecaliphs.edu.my>
2025-05-22 15:52:29 +0800

    Update choices.ts

--- src/data/choices.ts
+++ src/data/choices.ts
@@ -2,17 +2,17 @@ import type { Choices } from '../types';
 
 export const choices: Choices = {
   plus: [
-    { text: 'Very Inaccurate', score: 1, color: 1 },
-    { text: 'Moderately Inaccurate', score: 2, color: 2 },
-    { text: 'Neither Accurate Nor Inaccurate', score: 3, color: 3 },
-    { text: 'Moderately Accurate', score: 4, color: 4 },
-    { text: 'Very Accurate', score: 5, color: 5 }
+    { text: 'Strongly Disagree', score: 1, color: 1 },
+    { text: 'Disagree', score: 2, color: 2 },
+    { text: 'Neither Agree or Disagree', score: 3, color: 3 },
+    { text: 'Agree', score: 4, color: 4 },
+    { text: 'Strongly Agree', score: 5, color: 5 }
   ],
   minus: [
-    { text: 'Very Inaccurate', score: 5, color: 1 },
-    { text: 'Moderately Inaccurate', score: 4, color: 2 },
-    { text: 'Neither Accurate Nor Inaccurate', score: 3, color: 3 },
-    { text: 'Moderately Accurate', score: 2, color: 4 },
-    { text: 'Very Accurate', score: 1, color: 5 }
+    { text: 'Strongly Disagree', score: 5, color: 1 },
+    { text: 'Disagree', score: 4, color: 2 },
+    { text: 'Neither Agree or Disagree', score: 3, color: 3 },
+    { text: 'Agree', score: 2, color: 4 },
+    { text: 'Strongly Agree', score: 1, color: 5 }
   ]
 };
